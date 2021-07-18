47b87ac382158c42b2ffe1eb0261ce2b18f32508
SOWMYA0604 <85829094+SOWMYA0604@users.noreply.github.com>
2021-07-18 06:59:25 -0800

    Add files via upload

---
+++ building.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int i=1,n,mul;
+    scanf("%d",&n);
+    while (i<=10)
+    {
+       mul= n*i;
+       printf("%dx%d=%d\n",n,i,mul);
+       i++;
+       
+    }
+    
+        
+
+
+
+
+
+}
